2335b88535645954e409db75d3c51bf6db91d427
i.valuev <ivaluev@gmail.com>
2020-06-01 13:11:17 +0300

    added storybook

--- src/packages/button-c-menu.stories.tsx
+++ src/packages/button-c-menu.stories.tsx
@@ -1,11 +1,10 @@
-export {};
-// import React from 'react';
-// import ButtonCMenu from './button-c-menu';
-// import { action } from '@storybook/addon-actions';
+import React from 'react';
+import ButtonCMenu from './button-c-menu';
+import { action } from '@storybook/addon-actions';
 
-// export default {
-//   title: 'Button-C-Menu',
-//   component: ButtonCMenu
-// }
+export default {
+  title: 'Button-C-Menu',
+  component: ButtonCMenu
+};
 
-// export const Normal = () => <ButtonCMenu onClick={action('menu clicked')} />
+export const Normal = () => <ButtonCMenu onClick={action('menu clicked')} />
--- src/packages/button-c-stop.stories.tsx
+++ src/packages/button-c-stop.stories.tsx
@@ -1,12 +1,11 @@
-export {};
-// import React from 'react';
-// import ButtonCStop from './button-c-stop';
-// import { action } from '@storybook/addon-actions';
+import React from 'react';
+import ButtonCStop from './button-c-stop';
+import { action } from '@storybook/addon-actions';
 
 
-// export default {
-//   title: 'Button-C-Stop',
-//   component: ButtonCStop
-// };
+export default {
+  title: 'Button-C-Stop',
+  component: ButtonCStop
+};
 
-// export const Normal = () => <ButtonCStop onClick={action('clicked')} />;
+export const Normal = () => <ButtonCStop onClick={action('clicked')} />;
