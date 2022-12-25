fe1f362fd3782135c261ad9b67212d2a13625ced
i.valuev <ivaluev@gmail.com>
2022-12-25 22:19:38 +0100

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import {Provider} from 'react-redux'
 import './index.css'
 // import './index.svg'
 import App from './modules/app'
-import * as serviceWorker from './serviceWorker'
 import {configureStore} from './store'
 
 const store = configureStore()
@@ -14,8 +13,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
